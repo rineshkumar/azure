43015c5a85388fcb3c13a9d5d85e566ab8a06b46
Rinesh Kumar <rineshkumar@gmail.com>
2020-09-28 21:32:35 +0530

    Changes to upload file

--- blobclient/Program.cs
+++ blobclient/Program.cs
@@ -14,13 +14,23 @@ namespace mrrkazure.blobclient
             string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
             //Create blob service client using conneciton string 
             BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-            string containerName= "mrrkcontainer"+Guid.NewGuid();
+            string containerName = "mrrkcontainer" + Guid.NewGuid();
             //Create container using blob service client and get blob container client in return 
             BlobContainerClient blobContainerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
-            System.Console.WriteLine( blobContainerClient.ToString());
-            System.Console.WriteLine(   "Press to continue");
-            Console.ReadLine();
-                
+            System.Console.WriteLine(blobContainerClient.ToString());
+            System.Console.WriteLine("Press to continue");
+            // Console.ReadLine();
+            string localPath = "./data";
+            string fileName = "mrrkfile" + Guid.NewGuid();
+            string localFilePath = Path.Combine(localPath + fileName);
+            await File.WriteAllTextAsync(localFilePath, "Hello World");
+            var blobClient = blobContainerClient.GetBlobClient(fileName);
+            System.Console.WriteLine("Uploading {0} to blob storage as {1} ", fileName, blobClient.Uri);
+            using (var fileStreamForupload = File.OpenRead(localFilePath))
+            {
+                await blobClient.UploadAsync(fileStreamForupload, true);
+            }
+            
         }
     }
 }
