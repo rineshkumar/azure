d530b9568dab439ed6cf3b224f20780e88d0187c
Rinesh Kumar <rineshkumar@gmail.com>
2020-09-28 21:20:18 +0530

    Creating a container using blog service client

--- blobclient/Program.cs
+++ blobclient/Program.cs
@@ -10,7 +10,17 @@ namespace mrrkazure.blobclient
     {
         public static async Task Main()
         {
+            //Get the connection string 
             string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+            //Create blob service client using conneciton string 
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            string containerName= "mrrkcontainer"+Guid.NewGuid();
+            //Create container using blob service client and get blob container client in return 
+            BlobContainerClient blobContainerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+            System.Console.WriteLine( blobContainerClient.ToString());
+            System.Console.WriteLine(   "Press to continue");
+            Console.ReadLine();
+                
         }
     }
 }
